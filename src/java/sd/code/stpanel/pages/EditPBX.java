4c274b5a8a8d8c9e3d834a61421cb30bc6e6713f
motaz <motaz@L40-laptop>
2017-02-13 11:46:02 +0300

    cdrkeyfield

--- src/java/sd/code/stpanel/pages/EditPBX.java
+++ src/java/sd/code/stpanel/pages/EditPBX.java
@@ -170,7 +170,7 @@ public class EditPBX extends HttpServlet {
 			    "cdruser=\n" +
 			    "cdrpass=\n" +
 			    "cdrtable=\n" +
-                            "cdrfirstfield=";
+                            "cdrkeyfield=";
 	      }
 	      out.println("<tr><td>Remote STAgent config</td>");
 	      out.println("<td><textarea cols=80 rows=8 name=remoteconfig >" + result + "</textarea></td></tr>");
