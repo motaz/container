04c0ea0ba2e27b3dc1fab516143cc3aa77005379
motaz <motaz@L40-laptop>
2017-05-03 13:03:16 +0300

    EndOfToday function

--- src/sd/code/container/date/DateExtra.java
+++ src/sd/code/container/date/DateExtra.java
@@ -45,4 +45,15 @@ public class DateExtra {
     }
     
     
+    public static Date getEndOfToday() {
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, 23);
+        calendar.set(Calendar.MINUTE, 59);
+        calendar.set(Calendar.SECOND, 59);
+        calendar.set(Calendar.MILLISECOND, 999);
+
+        return  calendar.getTime();
+    }    
+    
 }
