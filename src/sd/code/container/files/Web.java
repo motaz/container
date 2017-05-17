d03ea2b9139b25ba9faadfd17fc4ac9d44517f45
motaz <motaz@L40-laptop>
2017-05-17 09:57:22 +0300

    Refactory: Introduce CodeURL in url package instead of files.Web

--- src/sd/code/container/files/Web.java
+++ src/sd/code/container/files/Web.java
@@ -5,24 +5,9 @@
  */
 package sd.code.container.files;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.net.URLConnection;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSession;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
+import sd.code.container.url.CodeURL;
 
 /**
  *
@@ -32,125 +17,27 @@ public class Web {
     
     public static String URLAddSlash(String url){
         
-        if ((url != null) && (!url.endsWith("/"))){
-              url = url + "/";
-        }
-        return url;
+       return CodeURL.URLAddSlash(url);
+       
     }
     
-    public static String callURL(String methodURL, String contents, int waitSeconds, String contentType) throws 
-	    IOException, MalformedURLException {
-	
-	if ((contentType == null) || (contentType.isEmpty())){
-	    contentType = "text/json";
-	}
-    
-        URL url = new URL(methodURL);
-        URLConnection conn = url.openConnection();
-        conn.setConnectTimeout(waitSeconds * 1000);            
-        conn.setReadTimeout(waitSeconds * 1000);        
-        conn.setRequestProperty("Content-Type", contentType);
-        conn.setDoOutput(true);
-        
-        return actualCall(conn, contents, "UTF-8");
+    public static String callURL(String methodURL, String contents, int waitSeconds, String contentType) throws IOException { 
+            
+	  return CodeURL.callURL(methodURL, contents, waitSeconds, contentType);
     }     
 
     public static String callURLWithMethod(String methodURL, String contents, int waitSeconds, String contentType, 
             String method, String encoding) throws IOException {
 	
-	if ((contentType == null) || (contentType.isEmpty())){
-	    contentType = "text/json";
-	}
-    
-        URL url = new URL(methodURL);
+	return CodeURL.callURLWithMethod(methodURL, contents, waitSeconds, contentType, method, encoding);
         
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setConnectTimeout(waitSeconds * 1000);            
-        conn.setReadTimeout(waitSeconds * 1000);        
-        conn.setRequestProperty("Content-Type", contentType);
-       
- 
-        
-        conn.setRequestProperty("method", method);
-        conn.setRequestMethod(method);
-        conn.setDoOutput(true);
-        
-        return actualCall(conn, contents, encoding);
     }         
     
     public static String actualCall(URLConnection conn, String contents, String encode) throws IOException {
         
-        String outputText = "";
-        BufferedReader reader;
-        try (OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream(), encode)) {
-            writer.write(contents);
-            
-            writer.flush();
-            String line;
-            
-            reader = new BufferedReader(new InputStreamReader(conn.getInputStream(), encode));
-            while ((line = reader.readLine()) != null) {
-                String text = new String(line.getBytes(encode));
-                outputText = outputText + text;
-                
-            }
-        }
-        
-        reader.close();
-        return outputText;
+       return CodeURL.actualCall(conn, contents, encode);
+       
     }     
-    
-    
-    public static String callHTTPSURLWithMethod(String methodURL, String contents, int waitSeconds, String contentType,
-            String method, String encoding) throws IOException {
-        
-        disableSslVerification();
-        return callURLWithMethod(methodURL, contents, waitSeconds, contentType, method, encoding);
-        
-    }
-    
-    public static boolean disableSslVerification() {
-        try {
-            // Create a trust manager that does not validate certificate chains
-            TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
-                @Override
-                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                    return null;
-                }
-
-                @Override
-                public void checkClientTrusted(X509Certificate[] certs, String authType) {
-                }
-
-                @Override
-                public void checkServerTrusted(X509Certificate[] certs, String authType) {
-                }
-               }
-            };
-
-            // Install the all-trusting trust manager
-            SSLContext sc = SSLContext.getInstance("SSL");
-            sc.init(null, trustAllCerts, new java.security.SecureRandom());
-            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-
-            // Create all-trusting host name verifier
-            HostnameVerifier allHostsValid = new HostnameVerifier() {
-                @Override
-                public boolean verify(String hostname, SSLSession session) {
-                    return true;
-                }
-
-            };
-
-            // Install the all-trusting host verifier
-            HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
-            return true;
-            
-        } catch (Exception  e) {
-            
-            return false;
-        }
-    } // end  disableSslVerification function
     
     
 }
---
+++ src/sd/code/container/url/CodeURL.java
@@ -0,0 +1,153 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sd.code.container.url;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.security.cert.X509Certificate;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+/**
+ *
+ * @author motaz
+ */
+public class CodeURL {
+    
+    public static String URLAddSlash(String url){
+        
+        if ((url != null) && (!url.endsWith("/"))){
+              url = url + "/";
+        }
+        return url;
+    }
+    
+    public static String callURL(String methodURL, String contents, int waitSeconds, String contentType) throws 
+	    IOException, MalformedURLException {
+	
+	if ((contentType == null) || (contentType.isEmpty())){
+	    contentType = "text/json";
+	}
+    
+        URL url = new URL(methodURL);
+        URLConnection conn = url.openConnection();
+        conn.setConnectTimeout(waitSeconds * 1000);            
+        conn.setReadTimeout(waitSeconds * 1000);        
+        conn.setRequestProperty("Content-Type", contentType);
+        conn.setDoOutput(true);
+        
+        return actualCall(conn, contents, "UTF-8");
+    }     
+
+    public static String callURLWithMethod(String methodURL, String contents, int waitSeconds, String contentType, 
+            String method, String encoding) throws IOException {
+	
+	if ((contentType == null) || (contentType.isEmpty())){
+	    contentType = "text/json";
+	}
+    
+        URL url = new URL(methodURL);
+        
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setConnectTimeout(waitSeconds * 1000);            
+        conn.setReadTimeout(waitSeconds * 1000);        
+        conn.setRequestProperty("Content-Type", contentType);
+       
+ 
+        
+        conn.setRequestProperty("method", method);
+        conn.setRequestMethod(method);
+        conn.setDoOutput(true);
+        
+        return actualCall(conn, contents, encoding);
+    }         
+    
+    public static String actualCall(URLConnection conn, String contents, String encode) throws IOException {
+        
+        String outputText = "";
+        BufferedReader reader;
+        try (OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream(), encode)) {
+            writer.write(contents);
+            
+            writer.flush();
+            String line;
+            
+            reader = new BufferedReader(new InputStreamReader(conn.getInputStream(), encode));
+            while ((line = reader.readLine()) != null) {
+                String text = new String(line.getBytes(encode));
+                outputText = outputText + text;
+                
+            }
+        }
+        
+        reader.close();
+        return outputText;
+    }     
+    
+    
+    public static String callHTTPSURLWithMethod(String methodURL, String contents, int waitSeconds, String contentType,
+            String method, String encoding) throws IOException {
+        
+        disableSslVerification();
+        return callURLWithMethod(methodURL, contents, waitSeconds, contentType, method, encoding);
+        
+    }
+    
+    public static boolean disableSslVerification() {
+        try {
+            // Create a trust manager that does not validate certificate chains
+            TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+                @Override
+                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+
+                @Override
+                public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                }
+
+                @Override
+                public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                }
+               }
+            };
+
+            // Install the all-trusting trust manager
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+            // Create all-trusting host name verifier
+            HostnameVerifier allHostsValid = new HostnameVerifier() {
+                @Override
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+
+            };
+
+            // Install the all-trusting host verifier
+            HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+            return true;
+            
+        } catch (Exception  e) {
+            
+            return false;
+        }
+    } // end  disableSslVerification function
+        
+    
+}
