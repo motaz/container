2bc4ad65549ee6a90e209377b602f241132be58d
motaz <motaz@L40-laptop>
2017-05-21 15:16:20 +0300

    Web.ReadHTTPRequest

--- src/sd/code/container/files/Web.java
+++ src/sd/code/container/files/Web.java
@@ -5,6 +5,7 @@
  */
 package sd.code.container.files;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.net.URLConnection;
 import sd.code.container.url.CodeURL;
@@ -39,7 +40,17 @@ public class Web {
         
        return CodeURL.actualCall(conn, contents, encode);
        
-    }     
+    }  
+    
+    public static String readHTTPRequest(BufferedReader reader) throws IOException{
+        
+        String line;
+        String data = "";
+        while ((line = reader.readLine()) != null){
+            data = data + line;
+        }    
+        return data;
+    }
     
     
 }
